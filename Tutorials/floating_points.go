12b7a301d6849bf3d52dfaf292074750f9b38852
Ramnaresh Ulaganathan <121196326+ramnareeesh@users.noreply.github.com>
2024-01-17 19:07:11 +0400

    basic datatypes in go - floating points

---
+++ Tutorials/floating_points.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "math"
+
+func main() {
+	var num float32 = 3.0999
+	fmt.Println(num)
+
+	fmt.Println("Maximum value for float 64", math.MaxFloat64)
+	// this is imported from the math library to show the max value of a fp number
+}
