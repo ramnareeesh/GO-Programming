cb8cbf0ff5988e3ec087acd3946713a520f8cf83
Ramnaresh Ulaganathan <121196326+ramnareeesh@users.noreply.github.com>
2024-01-17 18:59:22 +0400

    basic datatypes in go - integers

--- Tutorials/basic_datatypes.go
+++ Tutorials/basic_datatypes.go
@@ -9,4 +9,12 @@ func main() {
 	fmt.Println(num1, num2)
 	fmt.Printf("%T\n", num1)
 	fmt.Printf("%T\n", num2)
+
+	//rune := 'G' // rune is a alias for int32 which returns the unicode of the character
+	//fmt.Println(rune)
+
+	var unsigned uint = -10
+	// this throws a compilation error
+	// variable overflows because uint declaration
+	fmt.Println(unsigned)
 }
